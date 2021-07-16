e26d64f52feee9a77836a175449a0d1a871ec6a6
rikicop <ruperto1@protonmail.com>
2021-07-16 16:49:55 -0400

    card project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
+/* import moment from 'moment'; */
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Avatar({ hash }) {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const url = `http://www.pngmart.com/files/${hash}`;
+  return (
+    <img src={url}
+      className="imagen"
+      alt="avatar" />
+  )
+}
+Avatar.propTypes = {
+  hash: PropTypes.string
+};
+
+function Nombre({ txt }) {
+  return (
+    <div className="nombre">
+      {txt}
+    </div>
+  )
+}
+
+Nombre.propTypes = {
+  txt: PropTypes.string
+};
+
+
+function Tarjeta({ files }) {
+  
+  return (
+
+    <div className="row">
+      {files.map(file=>( 
+           <div className="column" key={file.id}>
+            <div className="card"  >
+              <Avatar hash={file.gravatar}/>
+              <p>{file.titulo}</p>
+            </div>
+            </div>
+         ))}
+      
+    </div>
+
+  );
+}
+
+Tarjeta.propTypes = {
+  files:PropTypes.array
+
+};
+
+
+
+const testTarjeta = [
+  {
+    id: 1,
+    gravatar: "17/The-Simpsons-Cartoon-Transparent-Background.png",
+    titulo:"Bart"
+    
+  },
+  {
+    id: 2,
+    gravatar: "17/The-Simpsons-Cartoon-Transparent-PNG.png",
+    titulo:"Lisa"
+    
+  },
+
+  {
+    id: 3,
+    gravatar: "17/The-Simpsons-Cartoon-PNG-Image.png",
+    titulo:"Simpson Family"
+   
+  },
+];
+
+
+ReactDOM.render(<Tarjeta files={testTarjeta} />, document.querySelector('#root'));
